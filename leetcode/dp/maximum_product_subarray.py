0d571f78621e7beb5c8f1b4dc43379994f562535
kingkaushalagarwal <kaushalagarwal1997@gmail.com>
2020-08-25 00:11:40 +0530

    programming practice

---
+++ leetcode/dp/maximum_product_subarray.py
@@ -0,0 +1,12 @@
+#very good concept implemetation
+class Solution:
+    def maxProduct(self, A) :
+        B = [x for x in A[::-1]]
+        for i in range(1, len(A)):
+            A[i] *= A[i - 1] or 1
+            B[i] *= B[i - 1] or 1
+        return max(A+B)
+
+nums=[2,3,-2,4]
+ans = Solution().maxProduct(nums)
+print(ans)

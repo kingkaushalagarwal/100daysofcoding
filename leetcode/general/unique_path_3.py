81be4dc93820fcdc4a3520d99a00a80827087138
kingkaushalagarwal <kaushalagarwal1997@gmail.com>
2020-09-23 23:48:50 +0530

    programming practice

---
+++ leetcode/general/unique_path_3.py
@@ -0,0 +1,33 @@
+import sys
+
+sys.setrecursionlimit(10000)
+
+
+class Solution(object):
+    def uniquePathsIII(self, A):
+        self.res  =0
+        m ,n , empty = len(A),len(A[0]),1
+        for i in range(m):
+            for j in range(n):
+                if A[i][j]==1:
+                    x,y = (i,j)
+                elif A[i][j]==0:
+                    empty+= 1
+        def dfs(x,y,empty):
+            if not(0<=x<m and 0<=y<n and A[x][y]>=0 ): return
+            if A[x][y]==2:
+                if empty==0:
+                    self.res +=  1
+                return
+            A[x][y]=-2
+            dfs(x + 1,y,empty-1)
+            dfs(x - 1, y, empty - 1)
+            dfs(x , y + 1, empty - 1)
+            dfs(x , y - 1, empty - 1)
+            A[x][y] = 0
+        dfs(x,y,empty)
+        return self.res
+
+grid = [[1,0,0,0],[0,0,0,0],[0,0,2,-1]]
+ans  = Solution().uniquePathsIII(grid)
+print(ans)
